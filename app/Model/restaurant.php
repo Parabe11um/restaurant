3e5a7530aa4e9ff947c459679520a31c3c4895a3
SID400 <p200300@mail.ru>
2020-05-21 12:10:02 +0300

    add rules in model restaurant.php

--- app/Model/restaurant.php
+++ app/Model/restaurant.php
@@ -17,4 +17,15 @@ class restaurant extends Model
         'r_email',
         'r_tables',
     ];
+
+    const rules = [
+        'r_name' => 'Required|max:50|min:2',
+        'r_description' => 'Required|max:10000|min:10',
+        'r_inn' => 'Required|size:12',
+        'r_phone' => 'size:10',
+        'r_address' => 'max:500|min:10',
+        'r_site' => 'url',
+        'r_email' => 'email:rfc,dns',
+        'r_tables' => 'Required|JSON',
+    ];
 }
