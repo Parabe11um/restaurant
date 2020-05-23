b138a3a79bb1cd17fdcb37a7f1ad4d941fca8d7d
Parabe11um <r.a.bazhenoff@gmail.com>
2020-05-24 02:21:59 +0300

    Добавил роутинг, главную страницу, сделал компоненты для части других разделов, скорректировал welcome.blade.php

---
+++ resources/js/components/FindRest/FindRest.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+class FindRest extends Component {
+    render() {
+        return (
+            <>
+                <Header />
+                <div>
+                    this is our restaraunts!
+                </div>
+                <Footer />
+            </>
+        );
+    }
+}
+
+export default FindRest;
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.options({ processCssUrls: false });
+
 mix.react('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+    });
