8d4303818d2c9297cf42b3cc506c82e9a26a7c23
Parabe11um <r.a.bazhenoff@gmail.com>
2020-06-06 21:29:12 +0300

    back to / route

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,13 @@
 |
 */
 
-Route::view('{path}', 'welcome');
+Route::get('/', function () {
+    return view('welcome');
+});
+
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
-Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
 Route::resource('UsePRofil','UseAdminController');
